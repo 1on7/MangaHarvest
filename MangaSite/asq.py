654ce4dc5fab63b56f51ee892b2d41da86c9314c
Tony <55198731+1on7@users.noreply.github.com>
2024-04-11 18:06:23 +0300

    Add files via upload

---
+++ MangaSite/asq.py
@@ -0,0 +1,156 @@
+import datetime
+import html
+import sys
+from os import path
+
+from bs4 import BeautifulSoup
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+import re
+import time
+from utils import date
+import json
+
+import aiohttp
+import asyncio
+
+async def fetch(url, method='GET', data=None, headers=None):
+    async with aiohttp.ClientSession() as session:
+        if method == 'GET':
+            async with session.get(url, headers=headers) as response:
+                return await response.text()
+        elif method == 'POST':
+            async with session.post(url, data=data, headers=headers) as response:
+                response_txt = await response.json()
+                return response_txt
+        else:
+            raise ValueError(f"Invalid HTTP method: {method}")
+          
+async def asq_latest_chapters(post_url):
+    headers = {
+        "accept": "application/json, text/javascript, */*; q=0.01",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "sec-ch-ua": "\"Not A(Brand\";v=\"99\", \"Opera\";v=\"107\", \"Chromium\";v=\"121\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-requested-with": "XMLHttpRequest",
+        "x-oxylabs-render":"html"
+    }
+    response_text = await fetch(post_url, method='GET', headers=headers)
+    if response_text:
+        # Parse the HTML content
+        soup = BeautifulSoup(response_text, 'html.parser')
+        
+        # Extracting src image
+        img_src = soup.find('div', class_='summary_image').img['src']
+        
+        manga_excerpt = html.unescape(soup.find('div', class_='post-content').p.get_text().strip())
+
+        # Find all list items containing chapter information
+        chapter_item = soup.find('li', class_='wp-manga-chapter')
+
+        # Extract number for each chapter
+        try:
+          # Extract only the numeric part of the chapter number
+            chapter_num = float(re.search(r'\d+', chapter_item.a.text.strip()).group())
+        except ValueError:
+            chapter_num = int(re.search(r'\d+', chapter_item.a.text.strip()).group())
+        return chapter_num, img_src, manga_excerpt
+    else:
+        print("Failed to retrieve data.")
+        return None
+
+async def asq_info(name):
+    url = "https://3asq.org/wp-admin/admin-ajax.php"
+    headers = {
+        "accept": "application/json, text/javascript, */*; q=0.01",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "sec-ch-ua": "\"Chromium\";v=\"122\", \"Not(A:Brand\";v=\"24\", \"Opera\";v=\"108\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-requested-with": "XMLHttpRequest"
+    }
+    data = {
+        "action": "wp-manga-search-manga",
+        "title": name
+    }
+
+    # Send a POST request to the specified URL
+    response = await fetch(url, method="POST", data=data, headers=headers)
+
+    # Check if the request was successful
+    if response:
+        # Extract information for the first manga
+        manga_info = response['data']
+        if manga_info:
+           manga_info = manga_info[0]
+           # Extracted manga information
+           title = manga_info['title']
+           post_url = manga_info['url']
+           info = await asq_latest_chapters(post_url)
+           summary = info[2]
+           latest_chapter = info[0]
+           cover_url = info[1]
+        
+           return {"title": title, "summary": summary, "cover": cover_url, "id": 0, "latest_chapter": int(latest_chapter), "post_url": post_url}
+        else:
+          return 'not found'
+    else:
+        return "Request failed with status code:", 404
+
+
+async def asq_chapter_imgs(chapter_url):
+    response_text = await fetch(chapter_url)
+    soup = BeautifulSoup(response_text, 'html.parser')
+    image_divs = soup.find_all("div", class_="page-break")
+    image_urls = [div.find('img')['src'].strip() for div in image_divs]
+    return image_urls
+
+async def asq_chapters(post_url):
+    headers = {
+        "content-type": "application/json",
+        "sec-ch-ua": "\"Not A(Brand\";v=\"99\", \"Chrome\";v=\"107\", \"Chromium\";v=\"121\"",
+        "sec-ch-ua-arch": "\"x86\"",
+        "sec-ch-ua-bitness": "\"64\"",
+        "sec-ch-ua-full-version": "\"107.0.5045.21\"",
+        "sec-ch-ua-full-version-list": "\"Not A(Brand\";v=\"99.0.0.0\", \"Chrome\";v=\"107.0.5045.21\", \"Chromium\";v=\"121.0.6167.160\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-model": "\"\"",
+        "sec-ch-ua-platform": "\"Windows\"",
+        "sec-ch-ua-platform-version": "\"10.0.0\"",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-requested-with": "XMLHttpRequest",
+    }
+    response_text = await fetch(post_url, method='GET', headers=headers)
+    if response_text:
+        soup = BeautifulSoup(response_text, 'html.parser')
+        chapter_items = soup.find_all('li', class_='wp-manga-chapter')
+        chapters_info = {}
+        for chapter in chapter_items:
+            try:
+                # Extract only the numeric part of the chapter number
+                chapter_num = float(re.search(r'\d+', chapter.a.text.strip()).group())
+            except ValueError:
+                chapter_num = int(re.search(r'\d+', chapter.a.text.strip()).group())
+            chapter_url = chapter.a['href']
+            release_date = chapter.find('span', class_='chapter-release-date').i.text
+            chapter_key = (chapter_num, 'asq')
+            team_name = "asq"
+            print(chapter_num)
+            chapter_number = chapter_num
+            chapters_info[chapter_key] = {"chapter": chapter_num, "teams": [{"team_name": team_name, "chapter_date": date.convert_arabic_date_to_numeric(release_date.replace('،', '')), "chapter_page": await asq_chapter_imgs(chapter_url)}]}
+            await asyncio.sleep(2)
+        return list(chapters_info.values())
+    else:
+        print("Failed to retrieve data.")
+        return None
---
+++ MangaSite/teamXnovel.py
@@ -0,0 +1,160 @@
+import datetime
+import sys
+from os import path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+import asyncio
+from aiohttp import ClientSession, ClientTimeout
+from bs4 import BeautifulSoup
+import re
+import json
+from utils import date
+
+async def fetch(url, method='GET', data=None, headers=None):
+    async with ClientSession() as session:
+        if method == 'GET':
+            async with session.get(url, headers=headers) as response:
+                return await response.text()
+        elif method == 'POST':
+            async with session.post(url, data=data, headers=headers) as response:
+                response_txt = await response.text()
+                return response_txt
+        else:
+            raise ValueError(f"Invalid HTTP method: {method}")
+
+async def teamXnovel_summary(post_url):
+  headers = {
+    "accept": "application/json, text/javascript, */*; q=0.01",
+    "accept-language": "en-US,en;q=0.9",
+    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "sec-ch-ua": "\"Chromium\";v=\"122\", \"Not(A:Brand\";v=\"24\", \"Opera\";v=\"108\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "x-requested-with": "XMLHttpRequest"
+  }
+
+  response_text = await fetch(post_url, method='GET', headers=headers)
+  # Parse the HTML content
+  soup = BeautifulSoup(response_text, 'html.parser')
+  pages = []
+  summary = soup.find('div', class_='review-content').p.get_text().strip()
+  # Find the pagination list
+  pagination_list = soup.find('ul', class_='pagination')
+  if pagination_list:
+    # Initialize an empty set to store unique page numbers
+    page_numbers = set()
+    # Initialize an empty list to store the links
+    page_links = []
+
+    # Find all list items within the pagination list
+    for li in pagination_list.find_all('li', class_='page-item'):
+        # Find the anchor tag within the list item
+        link = li.find('a')
+        if link:
+            # Extract the href attribute value (link URL)
+          href = link['href']
+          # Extract the page number from the href attribute
+          page_number = int(href.split('=')[-1])
+        # Check if the page number is not already encountered
+          if page_number not in page_numbers:
+              # Add the page number to the set of unique page numbers
+              page_numbers.add(page_number)
+              # Append the link URL and page number as a tuple to the list
+              page_links.append((page_number, href))
+
+    # Sort the list of links by page number
+    page_links.sort(key=lambda x: x[0])
+
+    # Extract only the link URLs from the sorted list
+    sorted_page_links = [link[1] for link in page_links]
+  else:
+    sorted_page_links = post_url
+  return summary, sorted_page_links
+
+async def teamXnovel_info(name):
+  url = f'https://teamxnovel.com/ajax/search?keyword={name}'
+  
+  headers = {
+    "accept": "application/json, text/javascript, */*; q=0.01",
+    "accept-language": "en-US,en;q=0.9",
+    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "sec-ch-ua": "\"Chromium\";v=\"122\", \"Not(A:Brand\";v=\"24\", \"Opera\";v=\"108\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "x-requested-with": "XMLHttpRequest"
+  }
+
+  response_text = await fetch(url, method='GET', headers=headers)
+  # Parse the HTML content
+  soup = BeautifulSoup(response_text, 'html.parser')
+  first_item = soup.find('ol', class_='list-group').find('li')
+  title = first_item.find('a', class_='"fw-bold')
+  post_url = first_item.find('div', class_='image-parent').a['href']
+  img_url = first_item.find('img')['src'].strip()
+  info = await teamXnovel_summary(post_url)
+  description = info[0]
+  latest_chapter = first_item.find('span', class_='badge').text.strip()
+  pages = []
+  pages.append(post_url + '?page=1')
+  for page in info[1]:
+    pages.append(page)
+  if latest_chapter.isdigit():
+    latest_chapter = int(latest_chapter)
+  elif latest_chapter:
+    latest_chapter = float(latest_chapter)
+  return {"title": title, "summary": description, "cover": img_url, "id": 0, "latest_chapter": latest_chapter, 'page_items': pages}
+
+async def teamXnovel_chapter_imgs(chapter_url):
+  response_text = await fetch(chapter_url)
+  soup = BeautifulSoup(response_text, 'html.parser')
+  image_divs = soup.find_all("div", class_="page-break")
+  image_urls = [div.find('img')['src'].strip() for div in image_divs]
+  return image_urls
+
+async def teamXnovel_chapters(pages):
+  headers = {
+    "accept": "application/json, text/javascript, */*; q=0.01",
+    "accept-language": "en-US,en;q=0.9",
+    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "sec-ch-ua": "\"Chromium\";v=\"122\", \"Not(A:Brand\";v=\"24\", \"Opera\";v=\"108\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "x-requested-with": "XMLHttpRequest"
+  }
+  chapters_info = {}
+  for page in pages:
+    response_text = await fetch(page, method='GET', headers=headers)
+    if response_text:
+          soup = BeautifulSoup(response_text, 'html.parser')
+          # Find all list items containing chapter information
+          chapter_items = soup.find_all('ul')[4]
+          chapter_items = chapter_items.find_all('li')
+          for chapter in chapter_items:
+              ch_num = chapter.find_all('div', class_='epl-num')[1].getText().strip()
+              if ch_num != '':
+                try:
+                    # Extract only the numeric part of the chapter number
+                    chapter_num = float(re.search(r'(\d+.\d+)', ch_num).group())
+                except:
+                    chapter_num = int(re.search(r'(\d+)', ch_num).group())
+              
+              chapter_url = chapter.a['href']
+              release_date = datetime.datetime.strptime(chapter.find('div', class_='epl-date eph-date d-none d-sm-block date-time').getText().strip(), '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d')
+              chapter_key = (chapter_num, 'asq')
+              team_name = "asq"
+              print(chapter_num)
+              chapter_number = chapter_num
+              chapters_info[chapter_key] = {"chapter": chapter_num, "teams": [{"team_name": team_name, "chapter_date": release_date, "chapter_page": await teamXnovel_chapter_imgs(chapter_url)}]}
+              await asyncio.sleep(2)
+    else:
+        print("Failed to retrieve data.")
+        return None
+  return list(chapters_info.values())
